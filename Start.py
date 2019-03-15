d6c228f02ea1af478fe400009e4535ba63579c9b
Oele Geirnaert <Oele.Geirnaert@medialaan.be>
2019-03-15 13:21:55 +0100

    Extended server

--- Start.py
+++ Start.py
@@ -7,7 +7,7 @@ import requests
 import sys
 
 from models import *
-from mongo_models import Freelancer, Mailing
+from models_mongo import Freelancer, Mailing, ContactApprover
 from config import ProgramConfig
 import dt_util
 
@@ -51,7 +51,7 @@ def get_one_user(config, the_user_id):
     my_config = config
     person = Person(config=my_config, id=the_user_id)
     person.get_from_dt()
-    print(person.get_full_name())
+    return person
 
 def random_password_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -62,20 +62,17 @@ def create_freelancers_from_file(config, from_person_id, a_file):
     copy_from_person = Person(config=config, id=from_person_id)
     copy_from_person.get_from_dt()
 
-    with open(a_file) as f:
-        content = f.readlines()
-    content = [x.strip() for x in content]
+    content = get_list_from_file(a_file, separator = ";")
 
     for current_line in content:
         unique_password = random_password_generator()
-        split = current_line.split(";")
-        print(split[4])
+        print(current_line[3])
         current_person = copy.copy(copy_from_person)
-        current_person.Username = split[0]
-        current_person.FirstName = split[1]
-        current_person.LastName =  split[2]
-        current_person.FullName = split[3]
-        current_person.ContactId = split[4]
+        current_person.Username = current_line[0]
+        current_person.FirstName = current_line[1]
+        current_person.LastName =  current_line[2]
+        current_person.FullName = current_line[3]
+        current_person.ContactId = current_line[4]
         current_person.Password = unique_password
         current_person.Id = None
 
@@ -105,10 +102,75 @@ def get_all_contacts_and_store_in_file(config, file):
         f.write(r.text)
     f.close()
 
-my_config = ProgramConfig("CreateFreelancers")
-get_all_persons_and_store_in_file(my_config, "OUTPUT_FILES/ids.txt")
+def get_all_holiday_approvers(config, file):
+    url = "%s/Contact/ReadLight" %config.domain
+    r = requests.post(url, headers = config.headers, cookies = config.cookies, data={'pagesize':0})
+    if not dt_util.logged_in(r.text, config):
+        print("You're not loggedin")
+        sys.exit()
+    json_list = json.loads(r.text)
+    print(r.text)
+    with open(file, 'w+') as f:
+        for item in json_list['Data']:
+            current_contact = Contact(config, Id=item['Id'])
+            current_contact.get_from_dt()
+            current_contact_name = current_contact.get_full_name()
+
+            current_approver_name = "has no approver"
+            current_approver_id = None
+            if current_contact.HolidayApproverId is not None:
+                current_approver = Contact(config, Id=current_contact.HolidayApproverId)
+                current_approver.get_from_dt()
+                current_approver_name = current_approver.get_full_name()
+                current_approver_id = current_contact.HolidayApproverId
+
+            f.write("%s;%s;%s;%s;%s;\n" %(current_contact.Id, current_contact_name, current_contact.ContactType, current_approver_id, current_approver_name ))
+            #sys.exit()
+    f.close()
+
+def update_contacts_with_approver_from_file(config, file):
+    db = config.mongodb_database
+    content = get_list_from_file(file, separator=";")
+
+    for current_item in content:
+        current_contact = Contact(config, Id=current_item[0])
+        current_contact.get_from_dt()
+        current_contact.HolidayApproverId = current_item[1]
+        print(current_contact.HolidayApproverId)
+        if current_contact.send_to_dt():
+            #Id, FullName, DepartmentGroup, Department, ApproverID, ApproverFullName
+            ca = ContactApprover(current_contact.Id, current_contact.get_full_name(), current_item[2], current_item[3], current_contact.HolidayApproverId, current_item[4])
+            ca.update_success(True)
+            db.contactapprover.update_one({'Id':ca.Id}, {'$set':ca.__dict__}, upsert=True)
+        #sys.exit()
+
+def get_list_from_file(file, separator = None):
+    my_list_to_return = []
+
+    with open(file) as f:
+        content = f.readlines()
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    content = [x.strip() for x in content]
+
+    if separator is not None:
+        print("Your lines will be separated with a: '%s'" %separator)
+        for current_line in content:
+            my_new_list = current_line.split(separator)
+            my_list_to_return.append(my_new_list)
+    else:
+        my_list_to_return = content[:]
+
+    return my_list_to_return
+
+my_config = ProgramConfig("GetAllHolidayApprovers")
+#x = get_list_from_file("INPUT_FILES/contacts_with_approver.txt", separator=";")
+#for i in x:
+    #print(i[1])
+#get_all_persons_and_store_in_file(my_config, "OUTPUT_FILES/ids.txt")
 #update_user_passwords_from_file(my_config, "TXT_FILES/password_person_id_to_update.txt")
 #update_user(my_config, 5)
 #get_one_user(my_config, 115)
 #create_freelancers_from_file(my_config, 116, "TXT_FILES/freelancers_to_create.txt")
 #get_all_contacts_and_store_in_file(my_config,"OUTPUT_FILES/dt_contacts.txt")
+#get_all_holiday_approvers(my_config, "OUTPUT_FILES/holiday_approvers.txt")
+#update_contacts_with_approver_from_file(my_config, "INPUT_FILES/contacts_with_approver.txt")
